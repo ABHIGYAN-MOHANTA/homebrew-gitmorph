8307a364ea86c909dbc483f6bb2a6f093409ff75
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2025-09-09 15:53:24 +0530

    Update description and improve build command

--- Formula/gitmorph.rb
+++ Formula/gitmorph.rb
@@ -1,17 +1,17 @@
 class Gitmorph < Formula
-  desc "Seamlessly switch between multiple Git identities with a Git wrapper"
+  desc "Switch between multiple Git profiles effortlessly 🚀"
   homepage "https://github.com/ABHIGYAN-MOHANTA/gitmorph"
   url "https://github.com/ABHIGYAN-MOHANTA/gitmorph/archive/refs/tags/v3.0.1.tar.gz"
-  sha256 "5e67d45ec279e303137981fac12a94f5598d8bf828c6f09d3417a4e334ed6b2d"
+  sha256 "f3619120d7e8af5adc120936798c27448df16ff5c2bb71d761078ff2e760fc01"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gitmorph"
+    system "go", "build", *std_go_args, "./cmd/gitmorph"
   end
 
   test do
-    system "#{bin}/gitmorph", "--help"
+    system "#{bin}/gitmorph", "--version"
   end
 end
